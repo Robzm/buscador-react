3dffbaa595d805c7262348fef561785995f5ebf1
Robzm <172452133+Robzm@users.noreply.github.com>
2025-05-18 18:34:59 -0500

    feat: add source and api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 
 //import './App.css'
+import { SearchBar } from "./components/SearchBar"
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  const filteredProducts = products.filter(product =>
+    product.title.toLowerCase().includes(search.toLowerCase())
+  );
   
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline bg-amber-300">
-        Hello world!
-      </h1>
+      <div class="w-56 h-56">
+        <h1>Productos</h1>
+        <SearchBar search={search} onSearch={setSearch} />
+      </div>
     </>
   )
 }
