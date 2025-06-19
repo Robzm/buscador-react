cf34c6cda4ad9a792accd5e5d631bcdc82534968
Robzm <172452133+Robzm@users.noreply.github.com>
2025-06-18 19:20:57 -0500

    feat: update header title to include 'De'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 export const Header = () => {
   return (
     <header >
-        <h1 className="font-mono text-6xl mb-6 mt-5">Buscador<strong>Productos</strong></h1>
+        <h1 className="font-mono text-6xl mb-6 mt-5">BuscadorDe<strong>Productos</strong></h1>
     </header>
   )
 }
